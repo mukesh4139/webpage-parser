6d303d773873b3264432eba4531e9d5728804a0a
Mukesh Chaudhary <mukesh.chaudhary@getfareye.com>
2019-04-04 23:07:44 +0530

    Add Http Url Validity Specs

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Page, type: :model do
   # Validation Test
   # ensure column url are present before saving
   it { should validate_presence_of(:url) }
+
+  # Format Validations
+  # Invalidity
+  it "invalidates invalid url" do
+    page = Page.new(url: "google")
+    page.valid?
+    expect(page.errors.messages[:url]).to match(["is not a valid HTTP URL"])
+  end
+
+  # Validity
+  it "validates valid url" do
+    page = Page.new(url: "https://wikipedia.org")
+    expect(page).to be_valid
+  end
 end
